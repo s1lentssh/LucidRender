d51668eb373e24394f6bb71c5d8a0a800e100837
s1lentssh <s1lent.ssh@gmail.com>
2023-02-26 17:47:27 +0100

    Fixed build on Windows

--- CMake/macros.cmake
+++ CMake/macros.cmake
@@ -1,6 +1,6 @@
 function(SetMaxWarningLevel Project)
     if(MSVC)
-        target_compile_options(${Project} PRIVATE /W4 /WX)
+        target_compile_options(${Project} PRIVATE /W3)
     else()
         target_compile_options(${Project} PRIVATE
         #[[ Interesting
@@ -37,6 +37,15 @@ function(SetMaxWarningLevel Project)
     endif()
 endfunction(SetMaxWarningLevel)
 
+function(SetWindowsVersion Project)
+    target_compile_definitions(${Project} PRIVATE _WIN32_WINNT=0x0601)
+endfunction(SetWindowsVersion)
+
 function(SetLucidVersion Project)
     target_compile_definitions(${Project} PRIVATE LUCID_RENDER_VERSION="${LUCID_RENDER_VERSION}")
 endfunction(SetLucidVersion)
+
+macro(GetWindowsVersion WINDOWS_VERSION)
+    string(REGEX MATCH "^([0-9]+)\\.([0-9]+)" WINDOWS_VERSION ${CMAKE_SYSTEM_VERSION})
+    math(EXPR WINDOWS_VERSION "(${CMAKE_MATCH_1} << 8) + ${CMAKE_MATCH_2}" OUTPUT_FORMAT HEXADECIMAL)
+endmacro(GetWindowsVersion)
